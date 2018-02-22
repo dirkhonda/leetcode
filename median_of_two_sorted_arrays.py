d44a6709a83cfd4a139d4b3566a55f7a421ab978
Dirk Honda <dirkhonda@gmail.com>
2018-02-22 14:21:27 -0800

    median of two sorted arrays

--- median_of_two_sorted_arrays.py
+++ median_of_two_sorted_arrays.py
@@ -1,50 +1,21 @@
-# There are two sorted arrays nums1 and nums2 of size m and n respectively.
-# Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
-
-# Example 1:
-#   nums1 = [1, 3], nums2 = [2]
-#   The median is 2.0
-#
-# Example 2:
-#   nums1 = [1, 2], nums2 = [3, 4]
-
-
-# simple case
-# edge case
-# null case
-
-def b_search(array, num, lo, hi):
-
-    if lo == hi:
-        return lo
-    mid = (lo + hi) // 2
-    if array[mid] == num:
-        return mid
-    if array[mid] > num:
-        return b_search(array, num, lo, mid-1)
-    if array[mid] < num:
-        return b_search(array, num, mid+1, hi)
+def findMedianSortedArrays(nums1, nums2):
+    nums1.extend(nums2)
+    nums1.sort()
+    length = len(nums1)
+    mid = length // 2
     
-
-def find_median(nums1, nums2):
-    for n in nums2:
-        index = b_search(nums1, n, 0, len(nums1))
-        nums1.insert(abs(index) + 1, n)
-    a_len = len(nums1)
-
-    if a_len % 2 == 0:
-        return (nums1[a_len//2] + nums1[a_len//2+1]) // 2
+    if not nums1:
+        return None
+    if len(nums1) == 1:
+        return nums1[0]
+    
+    if length % 2 == 0:
+        return (nums1[mid-1] + nums1[mid]) / 2
     else:
-         return nums1[a_len//2]
+        return nums1[mid]
 
-nums = []
-for i in range(1, 10):
-    nums.append(i)
-nums.remove(6)
 
-#print(b_search(nums, 6, 0, len(nums)))
-print(find_median([1, 3], [2, 4, 5, 1]))
-
-#print(b_search(nums, 1, 0, len(nums)))
-#print(b_search(nums, 8, 0, len(nums)))
-#print(nums[7])
+nums1 = [1, 3]
+nums2 = [2]
+# not optimal
+print(findMedianSortedArrays(nums1, nums2))
